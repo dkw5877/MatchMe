77e851901b9660162e1dd81cd452385c3eae1976
dkw5877 <dkw5877@gmail.com>
2014-06-17 09:41:20 -0700

    configure the collection view cells and delegate methods

--- MatchMe/GameBoardViewController.m
+++ MatchMe/GameBoardViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "GameBoardViewController.h"
+#import "PlayingCardCell.h"
 
 @interface GameBoardViewController ()
 
@@ -26,24 +27,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
 }
 
-- (void)didReceiveMemoryWarning
+- (NSInteger)numberOfPairs
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return 6;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    return 2 * [self numberOfPairs];
 }
-*/
+
+- (UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    PlayingCardCell* playingCardCell = [collectionView dequeueReusableCellWithReuseIdentifier:@"PlayingCardCell" forIndexPath:indexPath];
+    return playingCardCell;
+}
 
 @end
